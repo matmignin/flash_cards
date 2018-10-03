906c505df5f2ca8b0e2a4276ee48cb618df171b0
mat hough <mathough@gmail.com>
2018-10-03 15:38:08 -0400

    add txt file and read file commands

--- main.py
+++ main.py
@@ -2,6 +2,21 @@
 from click import Choice, command, option
 
 
+
+
+with open('patho study guide.txt', 'r') as f:
+    qList = f.read()
+    Question = (qList.split('\n\n'))
+
+print("There are",len(Question) - 1, "questions.")
+x = int(input("what question would you like to see?"))
+
+print(Question[x])
+
+
+
+
+"""
 cards = {
     "default": "DEFAULT",
     "hypocampus": "brain",
@@ -18,7 +33,7 @@ available_cards = cards.keys()
     "--card",
     help="The card we want to select",
     default="default",
-    type=Choice(available_cards),
+    # type=Choice(available_cards),
 )
 def cli(card):
     try:
@@ -29,6 +44,7 @@ def cli(card):
 if __name__ == "__main__":
     cli()
 
+"""
 
 
 
