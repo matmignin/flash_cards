4bdcc9c2fd58167e106c7a30ce84624cca817f66
mat hough <mathough@gmail.com>
2018-10-07 04:44:10 +0000

    add format function and review loop

--- main.py
+++ main.py
@@ -1,34 +1,67 @@
 #!/usr/bin/env python
 from click import Choice, command, option
+import re
 
-
-
+def PrintQuestion(str):
+    PostNum = "\. "
+    newstr = re.sub(PostNum, " ]\n", str, 1)
+    print("[",newstr, "\n")
 
 with open('patho study guide.txt', 'r') as f:
     qList = f.read()
     Question = (qList.split('\n\n'))
 print()
-print("There are",len(Question) - 1, "questions for ", Question[0])
-x = int(input("what question would you like to see?"))
-print()
+NumberOfQuestions = len(Question) - 1
+print("There are", NumberOfQuestions, "questions for ", Question[0])
+x = int(input("what question would you like to see? \n"))
 
 Review = []
 Know = []
 RUNNING = True
 
+
 while RUNNING:
-    print(Question[x])
+   # print(Question[x], "\n")
+    PrintQuestion(Question[x])
     Correct = input("correct?")
     if "y" in Correct.lower():
         Know.append(Question[x])
+        x += 1
     if "n" in Correct.lower():
         Review.append(Question[x])
+        x += 1
     elif "q" in Correct.lower():
         RUNNING = False
-    print(len(Review))
+   # print("number of questions to review [", len(Review), "]")
+    if x > NumberOfQuestions:
+        RUNNING = False
     print()
-    x += 1
+    
+NumberToReview = len(Review)    
+print("_____________________________________________________________")
+print("_____________________________________________________________")
+print("\n \t \t  Number of Questions to Review", NumberToReview, "\n")
+print("_____________________________________________________________")
+print("_____________________________________________________________")
+y = 0
+while NumberToReview > 0:
+    print (Review[y], "\n")
+    Correct = input("correct?")
+    if "y" in Correct.lower():
+        Review.pop(y)
+        NumberToReview -= 1
+        y -= 1
+        continue
+    if "n" in Correct.lower():
+        y += 1
+    elif "q" in Correct.lower():
+        break
+    if y > NumberToReview - 1:
+        y = 0
+    else:
+        continue 
 
+print("good job! \n youre done")
 
 """
 cards = {
