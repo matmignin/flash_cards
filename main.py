aa27635710386fd29e27fa49497aa6cb616c4fb2
mat hough <mathough@gmail.com>
2018-10-16 02:15:12 -0400

    fix click commands

--- main.py
+++ main.py
@@ -3,103 +3,93 @@ from click import Choice, command, option
 import re
 
 def PrintQuestion(str):
-    PostNum = "\. "
-    newstr = re.sub(PostNum, " ]\n", str, 1)
-    pstr = newstr.split("\n")
-    print("[",pstr[0])
-    print(pstr[1],"\n")
-    print("\t", pstr[2])
-    if len(pstr) > 3:
-        for i in range(3, len(pstr)):
-            print("\t", pstr[i])
+    post_number = "\. "
+    new_string = re.sub(post_number, " ]\n", str, 1)
+    part_of_string = new_string.split("\n")
+    print("[",part_of_string[0])
+    print(part_of_string[1],"\n")
+    print("\t", part_of_string[2])
+    if len(part_of_string) > 3:
+        for i in range(3, len(part_of_string)):
+            print("\t", part_of_string[i])
     print() 
 
+with open('patho.txt', 'r') as f:
+    list_of_questions = f.read()
+    question = (list_of_questions.split('\n\n'))
 
-with open('patho study guide.txt', 'r') as f:
-    qList = f.read()
-    Question = (qList.split('\n\n'))
-print()
-NumberOfQuestions = len(Question) - 1
-print("_____________________________________________________________")
-print("_____________________________________________________________")
-print("\t\t ", Question[0])
-print("\t\t\t[",NumberOfQuestions,"]")
-print("_____________________________________________________________")
-print("_____________________________________________________________")
-x = int(input("\n what question would you like to see? \n"))
-
-Review = []
-Know = []
-RUNNING = True
-
-
-while NumberOfQuestions >= x:
-    PrintQuestion(Question[x])
-    Correct = input("correct?")
-    if "y" in Correct.lower():
-        Know.append(Question[x])
-        x += 1
-    if "n" in Correct.lower():
-        Review.append(Question[x])
-        x += 1
-    elif "q" in Correct.lower():
-        break
+def start(x):
     print()
-    
-NumberToReview = len(Review)    
-print("_____________________________________________________________")
-print("\n \t \t  Number of Questions to Review", NumberToReview, "\n")
-print("_____________________________________________________________")
-y = 0
-while NumberToReview > 0:
-    # print (Review[y], "\n"
-    PrintQuestion(Review[y])
-    Correct = input("correct?")
-    if "y" in Correct.lower():
-        Review.pop(y)
-        NumberToReview -= 1
-        y -= 1
-        continue
-    if "n" in Correct.lower():
-        y += 1
-    elif "q" in Correct.lower():
-        break
-    if y > NumberToReview - 1:
-        y = 0
-    else:
-        continue 
+    number_of_questions = len(question) - 1
+    print("_____________________________________________________________")
+    print("_____________________________________________________________")
+    print("\t\t ", question[0])
+    print("\t\t\t[",number_of_questions,"]")
+    print("_____________________________________________________________")
+    print("_____________________________________________________________")
 
-print("good job! \n youre done")
+    review = []
+    know = []
+    RUNNING = True
 
-"""
-cards = {
-    "default": "DEFAULT",
-    "hypocampus": "brain",
-    "heart": "chest",
-    "eyeballs": "head",
-    "feet": "legs",
-}
+    while number_of_questions >= x:
+        PrintQuestion(question[x])
+        correct = input("correct?")
+        if "y" in correct.lower():
+            know.append(question[x])
+            x += 1
+        if "n" in correct.lower():
+            review.append(question[x])
+            x += 1
+        elif "q" in correct.lower():
+            break
+        print()
+        
+    number_to_review = len(review)    
+    print("_____________________________________________________________")
+    print("\n \t \t  Number of questions to review", number_to_review, "\n")
+    print("_____________________________________________________________")
+    y = 0
+    while number_to_review > 0:
+        # print (review[y], "\n"
+        PrintQuestion(review[y])
+        correct = input("correct?")
+        if "y" in correct.lower():
+            review.pop(y)
+            number_to_review -= 1
+            y -= 1
+            continue
+        if "n" in correct.lower():
+            y += 1
+        elif "q" in correct.lower():
+            break
+        if y > number_to_review - 1:
+            y = 0
+        else:
+            continue 
 
-available_cards = cards.keys()
+    print("good job! \n youre done")
+    return
+
+
 
 
 @command()
 @option(
-    "--card",
-    help="The card we want to select",
-    default="default",
-    # type=Choice(available_cards),
+    "--n",
+    help="The question we want to select",
+    default=1,
+    show_default=True,
+    type=int
 )
-def cli(card):
+def cli(n):
     try:
-        print(cards[card])
+        start(n)
     except KeyError:
-        print("thats not a thing")
+        print("thats not a thing you can do")
 
 if __name__ == "__main__":
     cli()
-
-"""
 
 
 
