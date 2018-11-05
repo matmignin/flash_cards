2f1f93427a2d332eaf7c685b0200d2bfbc4b01c1
mat hough <mathough@gmail.com>
2018-11-05 12:44:14 -0500

    implement html form via flask_wtf and made LoginForm class for managing username

--- webapp.py
+++ webapp.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+import main 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'kilgore'       
+
+class LoginForm(FlaskForm):
+    username = StringField('username')
+    password = PasswordField('password')
+    question = StringField('question')
+
+@app.route('/quiz', methods=['GET', 'POST'])
+def quiz():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        return 'the username is: {}.<br> the pasword is: {}.'.format(form.username.data, form.password.data)    
+    return render_template('patho.html', form=form)
 
 
-@app.route('/')
-def guide(name=None):
-    return render_template('patho.html', name=name)
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
