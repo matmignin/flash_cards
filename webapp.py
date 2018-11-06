caa5de879b0111ab40f9e302d3a4a4c3bbe78fa9
mat hough <mathough@gmail.com>
2018-11-05 21:08:33 -0500

    add form to output question number entered

--- webapp.py
+++ webapp.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField
 import main 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'kilgore'       
 
-class LoginForm(FlaskForm):
+class QuizForm(FlaskForm):
     username = StringField('username')
-    password = PasswordField('password')
     question = StringField('question')
 
 @app.route('/quiz', methods=['GET', 'POST'])
 def quiz():
-    form = LoginForm()
+    form = QuizForm()
 
     if form.validate_on_submit():
-        return 'the username is: {}.<br> the pasword is: {}.'.format(form.username.data, form.password.data)    
-    return render_template('patho.html', form=form)
+        return 'hi {}.<br> the Question for{} is.<br><br>{}'.format(form.username.data, form.question.data, main.question[int(form.question.data)])    
+    return render_template('patho.html', form=form) 
 
 
 
