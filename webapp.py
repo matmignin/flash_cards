1222db5d82147168b85128033dc59f4ee1ab3d49
mat hough <mathough@gmail.com>
2018-11-14 01:23:33 -0500

    add total_questions to QuizForm class

--- webapp.py
+++ webapp.py
@@ -2,21 +2,24 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField
-import main 
+import convert 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'kilgore'       
 
 class QuizForm(FlaskForm):
-    username = StringField('username')
+    username = StringField('name')
     question = StringField('question')
-    title = main.question[0]
+    title = convert.question[0]
+    total_questions = convert.number_of_questions
+
 
 @app.route('/quiz', methods=['GET', 'POST'])
 def quiz():
     form = QuizForm()
 
     if form.validate_on_submit():
-        return 'hi {}.<br> the Question for {} is:<br><br>{}'.format(form.username.data, form.question.data, main.question[int(form.question.data)])    
+
+        return 'hi {}.<br><br><br>{}'.format(form.username.data,  convert.question[int(form.question.data)])    
     return render_template('patho.html', form=form) 
 
 
