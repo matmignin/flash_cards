51663ea9eab6f7cf0e9c2a962e02851891f5c8ec
mat hough <mathough@gmail.com>
2018-11-14 01:20:30 -0500

    add explaination comments and a total_questions variable

--- convert.py
+++ convert.py
@@ -4,7 +4,11 @@ import re
 with open('patho.txt', 'r') as f:
     list_of_questions = f.read()
     convert_html = re.sub('\n', '<br>', list_of_questions)
+        #replaces all the linefeeds to html linefeeds
     post_number = re.sub(r'(?<=\d)\.', '<br>', convert_html)
+        #adds a new line and deletes the period after the questions number
     question = (post_number.split('<br><br>'))
+        #splits the questions into a list with the coorisonding number. question [0] is the title of the file
 
-print(question[0])
+
+number_of_questions = len(question) - 1
