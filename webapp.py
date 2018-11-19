90b77f77e588c81d93a03efdd746eb8dbad94a5e
mat hough <mathough@gmail.com>
2018-11-18 20:56:03 -0500

    Add signup feature a new route for register form as well as validators and a remember me check box.

--- webapp.py
+++ webapp.py
@@ -1,26 +1,40 @@
 #!/usr/bin/env python
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email
 import convert 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'kilgore'       
 
-class QuizForm(FlaskForm):
-    username = StringField('name')
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired()])
+    password = PasswordField('password', validators=[InputRequired()])
+    remember = BooleanField('remember me')
     question = StringField('question')
     title = convert.question[0]
     total_questions = convert.number_of_questions
 
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message='invalid email')])
+    username = StringField('username', validators=[InputRequired()])
+    password = PasswordField('password', validators=[InputRequired()])
 
-@app.route('/quiz', methods=['GET', 'POST'])
-def quiz():
-    form = QuizForm()
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
 
     if form.validate_on_submit():
-
         return 'hi {}.<br><br>Here is question {}'.format(form.username.data, convert.question[int(form.question.data)])    
     return render_template('patho.html', form=form) 
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+
+    return render_template('signup.html', form=form)
+
+
 
 
 
