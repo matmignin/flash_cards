582483dc106904bbefce6bded2377aaa6fd20ae9
matmignin <mat@mignin.com>
2020-05-25 04:15:32 -0400

    add ability to local file import
    - add import os
    - add upload folder env var and app.config
    - add allowed extentions env var

--- quiz/__init__.py
+++ quiz/__init__.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
+
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
+# adds static/upload path onto the current path directory so that it wont save in the root
+UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static/uploads/')
+
+# os.makedirs(UPLOAD_FOLDER)
+ALLOWED_EXTENSIONS = {'txt', 'jpg', 'jpeg','pdf'}
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'kilgore'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['UPLOADED_PHOTOS_DEST'] = 'static/uploads'
-app.config['ALLOWED_EXTENSIONS'] = {'txt', 'jpg', 'jpeg','pdf'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['ALLOWED_EXTENSIONS'] = ALLOWED_EXTENSIONS
 # app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
