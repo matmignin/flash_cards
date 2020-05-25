ebd2cd69056130f0e67a2cdb3861bcc1697badaf
matmignin <mat@mignin.com>
2020-05-25 04:18:21 -0400

    create /static/uploads dir

--- quiz/templates/changename.sh
+++ quiz/templates/changename.sh
@@ -2,15 +2,6 @@
 cd $1
 rm -rf ../static/
 mv assets/ ../static/
+mkdir ../static/uploads
 sed -i'.bs' -e 's|assets/|static/|g' *.html
 rm -f *.html.bs
-# sed -i'.bs' -e 's|"/static/img/|"img/|g' *.html
-# rm -f *.html.bs
-# sed -i'.bs' -e 's|<img src/*.png|imgz|g' *.html
-#<img src="
-#      {{ url_for('\static'\, filename='\
-  #img/profile.png
-#      '\) }}" height="{{query_img_height}}" width="{{query_img_width}}">
-
-#+g' signup1.html > signup.html
-# rm -f *.html.bs
